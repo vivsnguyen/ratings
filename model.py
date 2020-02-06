f5198401d390af1bcbb64cf46cdc9e4c8702b4b1
Your Name <you@example.com>
2020-02-05 17:49:28 -0800

    server and templates setup

--- model.py
+++ model.py
@@ -47,13 +47,26 @@ class Rating(db.Model):
     __tablename__ = "ratings"
 
     rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    movie_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     score = db.Column(db.Integer, nullable=False)
 
+    # Define relationship to user
+    user = db.relationship("User", backref=db.backref("ratings", order_by=rating_id))
 
+    # Define relationship to movie
+    movie = db.relationship("Movie", backref=db.backref("ratings", order_by=rating_id))
+
+    def __repr__(self):
+        """provide helpful representation when printed."""
+
+        return f"""<Rating rating_id={self.rating_id}
+                    movie_id={self.movie_id}
+                    user_id={self.user_id}
+                    score={self.score}>"""
 ##############################################################################
 # Helper functions
+
 
 def connect_to_db(app):
     """Connect the database to our Flask app."""
--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import (Flask, render_template, redirect, request, flash, session)
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,7 +22,37 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+
+@app.route('/register', methods=["GET"])
+def register_form():
+
+    return render_template("register_form.html")
+
+
+@app.route('/register', methods=["POST"])
+def register_process():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    age = int(request.form.get('age'))
+    zipcode = request.form.get('zipcode')
+    # print(email)
+    # print("hello Line 38!!!!")
+    # print(User.query.get(email))
+    if User.query.get(email) is None:
+        user = User(email=email, password=password, zipcode=zipcode, age=age)
+        db.session.add(user)
+        db.session.commit()
+
+    return redirect('/')
+
+
+@app.route('/users')
+def user_list():
+    """Show list of users."""
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
 
 
 if __name__ == "__main__":
